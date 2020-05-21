9fb95f34d513507ed8f215ee88ba04c151fa526e
Mohamed Elorabi <mxmhmd@yahoo.com>
2020-05-21 11:13:49 -0300

    the page

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import Help from "../views/Help.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -10,16 +8,6 @@ const routes = [
         path: "/",
         name: "Home",
         component: Home
-    },
-    {
-        path: "/about",
-        name: "About",
-        component: About
-    },
-    {
-        path: "/help",
-        name: "Help",
-        component: Help
     }
 ];
 
